5683c2f035d405c23507c749c1ee7b443675e1af
James Hibbard <8717652+JHibbard@users.noreply.github.com>
2022-03-15 11:40:33 -0700

    updated example

--- src/fpipes/jobs/example.py
+++ src/fpipes/jobs/example.py
@@ -1,26 +1,79 @@
+from abc import ABC, abstractmethod
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+from pyspark.sql.types import StructType, StructField
+from pyspark.sql.types import StringType, DateType, TimestampType, IntegerType, FloatType, BooleanType
 from delta import DeltaTable
 
-Class Example(Job):
+
+# base class
+class Job(ABC):
+
+    def __init__(self, spark=None, init_conf=None):
+        self.spark = self._prepare_spark(spark)
+        if init_conf:
+            self.conf = init_conf
+        else:
+            self.conf = self._provide_config()
+
+    @staticmethod
+    def _prepare_spark(spark) -> SparkSession:
+        if not spark:
+            return SparkSession.builder.getOrCreate()
+        else:
+            return spark
+
+    def _provide_config(self):
+        return {}
+
+    @abstractmethod
+    def launch(self):
+        pass
+
+
+# schema file, e.g. ../schemas/invoice.py
+VERSION = '1.0.0'
+SCHEMA = StructType([
+    StructField('invoice_number', IntegerType(), True, {'comment': 'invoice number'}, ),
+    StructField('invoice_date', DateType(), True, {'comment': 'date invoice was received'}),
+    # ...
+])
+
+
+# job file
+class Example(Job):
 
     def launch(self):
-        # table config
+        # Feature Table Config
         table_name = self.conf['table_name']
+        table_comment = self.conf['table_comment']
         table_location = self.conf['table_location']
 
+        # Source Config
+        source = self.conf['source']
+        source_format = self.conf['source_format']
+
+        # Streaming Config
+        read_options = self.conf['read_options']
+        write_options = self.conf['write_options']
+        trigger = self.conf['trigger']
+
+        # Create Feature Table if Absent
         if not DeltaTable.isDeltaTable(self.spark, table_location):
-            # create table
+            # DeltaTable.createOrReplace(self.spark) \
             DeltaTable.createIfNotExists(self.spark) \
                 .tableName(table_name) \
                 .comment(table_comment) \
                 .location(table_location) \
                 .addColumn('datasource', StringType(), comment='name of input file record was sourced from') \
-                .addColumn('ingesttime', TimeStamp(), comment='timestamp for when record was ingested') \
+                .addColumn('ingesttime', TimestampType(), comment='timestamp for when record was ingested') \
                 .addColumns(SCHEMA) \
                 .addColumn('p_ingestdate', DateType(), comment='(partition column) date record was ingested') \
                 .partitionedBy('p_ingestdate') \
                 .execute()
 
-            example_query = self.spark.readStream \
+            # Update Feature
+            example_bronze_query = self.spark.readStream \
                 .format(source_format) \
                 .schema(SCHEMA) \
                 .options(**read_options) \
@@ -36,3 +89,9 @@ Class Example(Job):
                     .format('delta') \
                     .options(**write_options, path=table_location) \
                     .start()
+
+            example_bronze_query.status
+            example_bronze_query.processAllAvailable()
+            example_bronze_query.stop()
+            # example_bronze_query.status
+            # example_bronze_query.lastProgress()
